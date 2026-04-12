372393be319870c064971b3df82bd19c009ae0b9
Mostafa Islam Elsayed <m0islam@outlook.com>
2026-04-12 15:09:48 +0200

    date

--- constants/events.ts
+++ constants/events.ts
@@ -2,7 +2,7 @@ export const events = [
     {
         id: '1',
         title: 'FARO Technology Day 2025',
-        date: "November 14, 2025",
+        date: "November, 2025",
         description: '3D Geoscan, the authorized distributor of FARO, proudly hosted FARO Technology Day 2025 under the theme “Scan The Future.” The event brought together industry professionals to explore the latest FARO technologies through live demonstrations, real-world project case studies from Egypt, and hands-on experience with cutting-edge FARO devices—showcasing how advanced 3D scanning is shaping the future of measurement and digital reality',
         imagesPath: 'images/events/faro-technology-day-2025',
         images: [
@@ -15,7 +15,7 @@ export const events = [
     {
         id: '2',
         title: 'ConstraTech Conference',
-        date: "November 14, 2025",
+        date: "May, 2024",
         description: '3D Geoscan participated as an exhibitor at Constratech Conference, a leading platform focused on construction technology and innovation in Egypt. The conference allowed us to present our advanced 3D scanning solutions while engaging with industry professionals driving the future of smart and technology-driven construction.',
         imagesPath: 'images/events/constatech-conf',
         images: [
@@ -28,7 +28,7 @@ export const events = [
     {
         id: '3',
         title: 'Faculty of Archaeology, ASU',
-        date: "November 14, 2025",
+        date: "December, 2024",
         description: '3D Geoscan participated in the International Conference “Sustainable and Smart Museums: Strategies, Practices, and Challenges, and cultural institutions, held at faculty of Archaeology Ain Shams University The conference brought together experts and professionals to discuss innovative approaches for sustainability, digital transformation, and smart technologies in museums, highlighting the role of advanced 3D scanning and 3D documentation solutions in preserving cultural heritage.',
         imagesPath: 'images/events/faculty-archaeology',
         images: [
@@ -39,7 +39,7 @@ export const events = [
     {
         id: '4',
         title: 'Big 5 Construct Egypt',
-        date: "November 14, 2025",
+        date: "June, 2023",
         description: '3D Geoscan participated as an exhibitor at Big 5 Construct Egypt, the largest gathering of construction professionals in Egypt. The event provided an excellent platform to showcase our advanced 3D scanning solutions, connect with industry leaders, and support the ongoing digital transformation of the construction sector',
         images: [
             '/images/events/big-5/DSC_5082.jpg',
