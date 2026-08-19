9ba2a9595b006f5de4ff74a46a50ced657fabde7
mohhazem <moh.hazemhf@gmail.com>
2026-08-20 02:44:08 +0300

    fixed phone number

--- constants/contact.ts
+++ constants/contact.ts
@@ -1,8 +1,8 @@
 export const CONTACT_INFO = [
   { icon: "mail", label:"Email", text: "info@3dgeoscan.com", href: "mailto:info@3dgeoscan.com" },
   { icon: "phone", label:"Mobile", text: "+20 105 003 7275", href: "tel:+201050037275" },
-  { icon: "phone", label:"Phone", text: "+20 34 29 0191", href: "tel:+20342901991" },
-  { icon: "phone", label:"Phone", text: "+20 34 90 192", href: "tel:+2034290192" },
+  { icon: "phone", label:"Phone", text: "+20 34 29 0191", href: "tel:+2034290191" },
+  { icon: "phone", label:"Phone", text: "+20 34 29 0192", href: "tel:+2034290192" },
 //   { icon: "location", label:"Location", text: "Alexandria, Egypt", href: "https://www.google.com/maps/place/Alexandria,+Alexandria+Governorate,+Egypt"},
 ];
 
